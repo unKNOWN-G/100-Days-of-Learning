5f886569af8427d85812f31be4bcf7632539bb9f
unKNOWN-G <karnatisaivenkatagiri@gmail.com>
2022-07-02 16:20:38 +0530

    JS(DAY-16 IN 30 DAYS OF JS)

---
+++ JavaScript/30-Days-Of-JavaScript/Day-16/script.js
@@ -0,0 +1,268 @@
+// JSON
+
+/*
+JSON.parse() (JSON -> OBJECT)
+JSON.stringify()  (OBJECT -> JSON)
+*/
+
+console.group('Practise')
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`
+    
+    const usersObj = JSON.parse(usersText, (key, value) => {
+      let newValue =
+        typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+      return newValue
+    })
+    console.log(usersObj)
+
+    const users = {
+        Alex: {
+          email: 'alex@alex.com',
+          skills: ['HTML', 'CSS', 'JavaScript'],
+          age: 20,
+          isLoggedIn: false,
+          points: 30
+        },
+        Asab: {
+          email: 'asab@asab.com',
+          skills: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Redux',
+            'MongoDB',
+            'Express',
+            'React',
+            'Node'
+          ],
+          age: 25,
+          isLoggedIn: false,
+          points: 50
+        },
+        Brook: {
+          email: 'daniel@daniel.com',
+          skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+          age: 30,
+          isLoggedIn: true,
+          points: 50
+        },
+        Daniel: {
+          email: 'daniel@alex.com',
+          skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+          age: 20,
+          isLoggedIn: false,
+          points: 40
+        },
+        John: {
+          email: 'john@john.com',
+          skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+          age: 20,
+          isLoggedIn: true,
+          points: 50
+        },
+        Thomas: {
+          email: 'thomas@thomas.com',
+          skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+          age: 20,
+          isLoggedIn: false,
+          points: 40
+        },
+        Paul: {
+          email: 'paul@paul.com',
+          skills: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'MongoDB',
+            'Express',
+            'React',
+            'Node'
+          ],
+          age: 20,
+          isLoggedIn: false,
+          points: 40
+        }
+      }
+      
+      const txt_2 = JSON.stringify(users, undefined, 4)
+      console.log(txt_2) // text means JSON- because json is a string form of an object.
+
+    // Using a Filter Array with JSON.stringify
+    const user = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    country: 'Finland',
+    city: 'Helsinki',
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+    age: 250,
+    isLoggedIn: false,
+    points: 30
+    }
+    
+    const txt_1 = JSON.stringify(user,['firstName', 'lastName', 'country', 'city', 'age'],4)
+    console.log(txt_1)
+
+console.groupEnd('Practise')
+// EXERCISES
+// DATA
+
+const skills = ['HTML', 'CSS', 'JS', 'React','Node', 'Python']
+let age = 250;
+let isMarried = true
+const student = {
+  firstName:'Asabeneh',
+  lastName:'Yetayehe',
+  age:250,
+  isMarried:true,
+  skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}`
+
+console.group('Level-1')
+skill_json = JSON.stringify(student,['skills'],4)
+console.log(skill_json)
+console.log(JSON.stringify({age:age},undefined,4))
+console.log(JSON.stringify({isMarried:isMarried},undefined,4))
+console.log(student,undefined,4)
+console.groupEnd('Level-1')
+
+console.group('Level-2')
+console.log(JSON.stringify(student,['firstName','lastName','skills'],4))
+console.groupEnd('Level-2')
+
+console.group('Level-3')
+
+new_obj = JSON.parse(txt, (key,value) => {
+    return key=="skills"? value.length : value
+})
+
+skill_count = []
+for(let name of Object.keys(new_obj)){
+    let skill_c = {}
+    skill_c[name] = new_obj[name]["skills"]
+    skill_count.push(skill_c)
+}
+
+skill_count.sort(function(a,b){
+    return Object.values(b) - Object.values(a)
+})
+
+console.log(Object.keys(skill_count[0]))
+
+console.groupEnd('Level-3')
